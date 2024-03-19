04e42a74cf2bd0ba4f161a92f5b6e8deafa8bf63
Attraya Das <attrayaghoshdas@gmail.com>
2024-03-19 23:45:05 +0530

    Initial commit

---
+++ src/main/java/com/attraya/SpringDockerPracticeApplication.java
@@ -0,0 +1,21 @@
+package com.attraya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringDockerPracticeApplication {
+
+	@GetMapping("/greet")
+	public String greeting(){
+		return "Welcome to Spring Boot Application using Docker! Your application is running :)";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerPracticeApplication.class, args);
+	}
+
+}
